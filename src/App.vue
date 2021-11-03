6c78cc4253d90f48adba3ffcd447c933dfacea57
Quentin Guerrier <qguerrier@myc.doctor>
2021-11-03 13:24:25 +0100

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,92 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container-body">
+        <h1>Documentation</h1>
+        <p class="explication">Tu trouvera ici toute la documentation pour vuecyclopedie</p>
+        <h2>Buttons</h2>
+        <h3>Regular</h3>
+        <div class="row">
+            <en-button class="margin-element" type="primary" title="Primary"/>
+            <en-button class="margin-element" type="primary" title="Primary" iconsLeft="ti ti-plus"/>
+            <en-button class="margin-element" type="primary" title="Primary" iconsRight="ti ti-chevron-down"/>
+            <en-button type="primary-icon" icon="ti ti-dots-vertical"/>
+        </div>
+        <div class="row">
+            <en-button class="margin-element" type="secondary" title="Secondary"/>
+            <en-button class="margin-element" type="secondary" title="Secondary" iconsLeft="ti ti-plus"/>
+            <en-button class="margin-element" type="secondary" title="Secondary" iconsRight="ti ti-chevron-down"/>
+            <en-button type="secondary-icon" icon="ti ti-dots-vertical"/>
+        </div>
+        <div class="row">
+            <en-button class="margin-element" type="terciary" title="Terciary"/>
+            <en-button class="margin-element" type="terciary" title="Terciary" iconsLeft="ti ti-plus"/>
+            <en-button class="margin-element" type="terciary" title="Terciary" iconsRight="ti ti-chevron-down"/>
+            <en-button type="terciary-icon" icon="ti ti-dots-vertical"/>
+        </div>
+        <h3>Small</h3>
+        <div class="row">
+            <en-button class="margin-element" type="primary" small title="Primary"/>
+            <en-button class="margin-element" type="primary" small title="Primary" iconsLeft="ti ti-plus"/>
+            <en-button class="margin-element" type="primary" title="Primary" small iconsRight="ti ti-chevron-down"/>
+            <en-button type="primary-icon" small icon="ti ti-dots-vertical"/>
+        </div>
+        <div class="row">
+            <en-button class="margin-element" type="secondary" small title="Secondary"/>
+            <en-button class="margin-element" type="secondary" small title="Secondary" iconsLeft="ti ti-plus"/>
+            <en-button class="margin-element" type="secondary" title="Secondary" small iconsRight="ti ti-chevron-down"/>
+            <en-button type="secondary-icon" icon="ti ti-dots-vertical"/>
+        </div>
+        <div class="row">
+            <en-button class="margin-element" type="terciary" small title="Terciary"/>
+            <en-button class="margin-element" type="terciary" small title="Terciary" iconsLeft="ti ti-plus"/>
+            <en-button class="margin-element" type="terciary" title="Terciary" small iconsRight="ti ti-chevron-down"/>
+            <en-button type="terciary-icon" icon="ti ti-dots-vertical"/>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import EnButton from './components/actions/EnButton.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+        EnButton,
+    },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+body{
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
+.container-body{
+    width: 80vw;
+    margin-left: 10vw;
+    padding-top: 100px;
+    h2{
+        margin-bottom: 20px;
+    }
+    h3{
+        margin-bottom: 10px;
+        color: #71717A;
+        font-family: Inter;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 18px;
+        line-height: 28px;
+    }
+    p{
+        margin-bottom: 50px;
+    }
+    .row{
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        margin-bottom: 20px;
+        .margin-element{
+            margin-right: 15px;
+        }
+    }
 }
 </style>
---
+++ src/components/actions/EnButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button :class="small ? 'button-' + type + '--small' : 'button-'+ type +'--regular'">
+        <i v-if="iconsLeft" class="icon-left" :class="iconsLeft"></i>
+        <span v-if="title">{{ title }}</span>
+        <i v-if="iconsRight" class="icon-right" :class="iconsRight"></i>
+        <i v-if="icon" class="icon" :class="icon"></i>
+    </button>
+</template>
+<script lang="ts">
+export default {
+    name: "en-button",
+    props: {
+        title: String,
+        small: Boolean,
+        icon: String,
+        iconsLeft: String,
+        iconsRight: String,
+        type: {
+            default: "primary"
+        }
+    }
+}
+</script>
