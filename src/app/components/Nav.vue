2617891830584c33bb59e610d19b6c2aab0ae700
Quentin Guerrier <qguerrier@myc.doctor>
2021-11-11 23:10:55 +0100

    new files order

--- src/app/components/Nav.vue
+++ src/app/components/Nav.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="nav-app">
-    <div class="logo">
-      logo
-    </div>
+    <div class="logo">logo</div>
     <h4>components</h4>
     <div class="list">
-        <router-link :class="$route.name === 'button' ? 'selected' : ''" class="link" to="/button">Buttons</router-link>
-        <router-link :class="$route.name === 'chips' ? 'selected' : ''" class="link" to="/chips">Chips</router-link>
+      <router-link :class="$route.name === 'button' ? 'selected' : ''" class="link" to="/button">Buttons</router-link>
+      <router-link :class="$route.name === 'chips' ? 'selected' : ''" class="link" to="/chips">Chips</router-link>
     </div>
   </div>
 </template>
